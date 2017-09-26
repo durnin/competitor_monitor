15957a66a3c79efd892bcd07bb0655b81309736a
Rodrigo Martinez <durnin83@gmail.com>
2017-09-25 21:27:19 -0300

    Acceptance feature test for group of competitors creation

---
+++ spec/feature/competitor_group_create_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature "Create a group of competitor's products" do
+
+  def add_competitor(name, link, asin)
+    click_button 'Add Competitor'
+    within('#new_competitor') do
+      fill_in 'Name', with: name
+      fill_in 'Link', with: link if link
+      fill_in 'Product ASIN', with: asin if asin
+      click_button 'Create Competitor'
+      expect(page).to have_content "Competitor #{competitor1[:name]}"\
+                                   'successfully added'
+    end
+  end
+
+  given(:group_name) { Faker::Company.name }
+  given(:competitor1) do
+    { name: Faker::Company.name, link: Faker::Internet.url }
+  end
+  given(:competitor2) do
+    { name: Faker::Company.name, asin: Faker::Code.asin }
+  end
+
+  scenario 'creates a group with competitors' do
+    visit '/groups/index'
+    click_button 'New Group'
+    within('#new_group') do
+      fill_in 'Group Name', with: group_name
+      click_button 'Create Group'
+      expect(page).to have_content "Group #{group_name} successfully created"
+    end
+    click_link group_name # link to show the group
+    add_competitor(competitor1[:name], competitor1[:link], nil)
+    add_competitor(competitor1[:name], nil, competitor1[:asin])
+  end
+end
