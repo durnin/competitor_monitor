41471c24bbaeae9a17ab0bc1e8c504d0c5b92358
Rodrigo Martinez <durnin83@gmail.com>
2017-09-26 10:53:45 -0300

    Add controller and views for groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Controller for Groups of Competitors
+class GroupsController < ApplicationController
+  def show; end
+
+  def index
+    @groups = []
+  end
+end
