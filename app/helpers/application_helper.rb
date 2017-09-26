f1f38289b26bf05a6527011bf7ad53895ca4eece
Rodrigo Martinez <durnin83@gmail.com>
2017-09-26 10:41:38 -0300

    Add navigation menu to app with some options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@
 
 # Rails ApplicationHelper Module
 module ApplicationHelper
+  def nav_menu_items
+    [{ text: 'Home', link: root_path },
+     { text: 'Groups', link: groups_path }].map do |menu_item|
+
+      menu_item_link = menu_item[:link]
+      menu_item_classes = if current_page?(menu_item_link)
+                            'nav-item nav-link active'
+                          else
+                            'nav-item nav-link'
+                          end
+      link_to menu_item[:text], menu_item_link, class: menu_item_classes
+    end.join.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'groups#index'
+  resources :groups, only: %i[index show]
+
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 end
