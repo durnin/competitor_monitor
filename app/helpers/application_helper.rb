a3ab431ec3db6ccab8d4dd80cf4b5b8e5644e4e8
Rodrigo Martinez <durnin83@gmail.com>
2017-09-26 17:29:39 -0300

    Add alert messages to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,37 @@ module ApplicationHelper
       link_to menu_item[:text], menu_item_link, class: menu_item_classes
     end.join.html_safe
   end
+
+  def alert_messages(flash, model_object = nil)
+    model_errors = model_error_messages_list(model_object)
+    flash.map do |key, text|
+      alert_class = if key == :notice
+                      'alert alert-success'
+                    else
+                      'alert alert-danger'
+                    end
+      messages_html(alert_class, text, model_errors)
+    end.join.html_safe
+  end
+
+  def model_error_messages_list(model_object)
+    if model_object
+      content_tag(
+        :ul,
+        model_object.errors.full_messages.map { |msg| content_tag(:li, msg) }
+          .join.html_safe
+      )
+    else
+      ''
+    end
+  end
+
+  private
+
+  def messages_html(alert_class, general_msg, other_msgs)
+    content = content_tag(:strong, general_msg) + other_msgs
+    content_tag(:div, content,
+                class: alert_class,
+                role: 'alert')
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -13,4 +13,57 @@ RSpec.describe ApplicationHelper, type: :helper do
       )
     end
   end
+
+  context 'when trying to print alert messages' do
+
+    let(:model_instance) { double('some_model') }
+
+    before do
+      allow(model_instance).to receive_message_chain(:errors, :full_messages)
+        .and_return(['error 1', 'error 2', 'error 3'])
+    end
+
+    describe '#model_error_messages_list' do
+      it 'generates html for error messages as a list' do
+        expect(helper.model_error_messages_list(model_instance)).to eq(
+          '<ul><li>error 1</li><li>error 2</li><li>error 3</li></ul>'.html_safe
+        )
+      end
+
+      it 'generates empty html for nil model object' do
+        expect(helper.model_error_messages_list(nil)).to eq('')
+      end
+    end
+
+    describe '#alert_messages' do
+      def test_alert_messages(flash_object, html_text)
+        expect(helper.alert_messages(flash_object, model_instance)).to eq(
+          html_text.html_safe
+        )
+      end
+
+      it 'generates empty html for when no flash object was set' do
+        flash_object = {}
+        expect(helper.alert_messages(flash_object)).to eq('')
+      end
+
+      it 'generates html for success alert' do
+        test_alert_messages(
+          { notice: 'This is a notice.' },
+          '<div class="alert alert-success" role="alert"><strong>This is a '\
+          'notice.</strong><ul><li>error 1</li><li>error 2</li><li>error 3'\
+          '</li></ul></div>'
+        )
+      end
+
+      it 'generates html for danger alert' do
+        test_alert_messages(
+          { error: 'This is an error.' },
+          '<div class="alert alert-danger" role="alert"><strong>This is an '\
+          'error.</strong><ul><li>error 1</li><li>error 2</li><li>error 3'\
+          '</li></ul></div>'
+        )
+      end
+    end
+  end
 end
