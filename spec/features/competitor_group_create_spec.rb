86b4877145d26088850d3eb17375c3bb686b2f04
Rodrigo Martinez <durnin83@gmail.com>
2017-09-29 11:08:45 -0300

    Fix acceptance test with new changes

--- spec/features/competitor_group_create_spec.rb
+++ spec/features/competitor_group_create_spec.rb
@@ -10,10 +10,10 @@ feature "Create a group of competitor's products" do
       fill_in 'Name', with: name
       fill_in 'Link', with: link if link
       fill_in 'Product ASIN', with: asin if asin
-      click_button 'Create Competitor'
-      expect(page).to have_content "Competitor #{competitor1[:name]}"\
-                                   'successfully added'
     end
+    click_button 'Create Competitor'
+    expect(page).to have_content "Competitor #{competitor1[:name]}"\
+                                   'successfully added'
   end
 
   given(:group_name) { Faker::Company.name }
@@ -26,12 +26,12 @@ feature "Create a group of competitor's products" do
 
   scenario 'creates a group with competitors' do
     visit groups_path
-    click_button 'New Group'
+    click_link 'New Group'
     within('#new_group') do
-      fill_in 'Group Name', with: group_name
-      click_button 'Save'
-      expect(page).to have_content "Group #{group_name} successfully created"
+      fill_in 'Name', with: group_name
     end
+    click_button 'Save'
+    expect(page).to have_content "Group #{group_name} successfully created"
     click_link group_name # link to show the group
     add_competitor(competitor1[:name], competitor1[:link], nil)
     add_competitor(competitor2[:name], nil, competitor2[:asin])
