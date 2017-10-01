297cd6a234075384e5e6725697dcb2282ad1a596
Rodrigo Martinez <durnin83@gmail.com>
2017-10-01 19:54:47 -0300

    Create test skelleton for product data extraction from amazon

---
+++ spec/services/competitor_monitor_service_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CompetitorMonitorService do
+  context 'when extracts data from amazon' do
+    it 'records price in competitor'
+    it 'records title in competitor'
+    it 'records images in competitor'
+    it 'records features in competitor'
+    it 'records number of reviews in competitor'
+    it 'records best seller rank in competitor'
+    it 'records inventory in competitor'
+  end
+end
