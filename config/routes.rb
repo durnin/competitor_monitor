847f7b9b21f2eabab764cfa623907eea7503ec75
Rodrigo Martinez <durnin83@gmail.com>
2017-10-03 14:47:50 -0300

    Add routes for sidekiq monitoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+
+
 Rails.application.routes.draw do
   root to: 'groups#index'
   resources :groups, only: %i[show index new create edit] do
     resources :competitors, only: %i[new]
   end
   resources :competitors, only: %i[show create edit]
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
