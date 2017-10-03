a78fc6480d5e7591533021c55354a6f1e6591055
Rodrigo Martinez <durnin83@gmail.com>
2017-10-03 08:34:13 -0300

    2nd refactor on scraper selectors

--- lib/amazon/scraper.rb
+++ lib/amazon/scraper.rb
@@ -60,13 +60,23 @@ module Amazon
 
   # Product stock scraper
   class ProductStockScraper
+    ADD_TO_CART_FORM_ID = 'addToCart'
+    UPDATE_CART_LINK_ID = 'hlb-view-cart-announce'
+    SELECTOR_UPDATE_CART_LINK = "a##{UPDATE_CART_LINK_ID}"
+    UPDATE_CART_FORM_ID = 'activeCartViewForm'
+    QUANTITY_FIELD_NAME_REGEX = /quantity\./
+    UPDATE_CART_SUBMIT_BTN_NAME = 'submit.update'
+    SELECTOR_QUANTITY_FIELD =
+      'form#activeCartViewForm div.sc-list-item-content '\
+      'input[name^="quantity."]'
+
     def initialize(agent, product_page)
-      add_item_page = product_page.form_with(id: 'addToCart').submit
-      @cart_form = if add_item_page.search('a#hlb-view-cart-announce').any?
+      add_item_page = product_page.form_with(id: ADD_TO_CART_FORM_ID).submit
+      @cart_form = if add_item_page.search(SELECTOR_UPDATE_CART_LINK).any?
                      update_cart_page = agent.click(
-                       add_item_page.link_with(id: 'hlb-view-cart-announce')
+                       add_item_page.link_with(id: UPDATE_CART_LINK_ID)
                      )
-                     update_cart_page.form(id: 'activeCartViewForm')
+                     update_cart_page.form(id: UPDATE_CART_FORM_ID)
                    end
     end
 
@@ -75,10 +85,8 @@ module Amazon
       data = {}
       data[:inventory] =
         if updated_stock_page
-          update_cart_stock_to_max.search(
-            'form#activeCartViewForm div.sc-list-item-content '\
-            'input[name^="quantity."]'
-          ).attr('value').value.to_i
+          update_cart_stock_to_max.search(SELECTOR_QUANTITY_FIELD)
+                                  .attr('value').value.to_i
         end
       data
     end
@@ -87,23 +95,23 @@ module Amazon
 
     def update_cart_stock_to_max
       return unless @cart_form
-      qty_field = @cart_form.field_with(name: /quantity\./)
+      qty_field = @cart_form.field_with(name: QUANTITY_FIELD_NAME_REGEX)
       qty_field.value = 999
-      submit_btn = @cart_form.button_with(name: 'submit.update')
+      submit_btn = @cart_form.button_with(name: UPDATE_CART_SUBMIT_BTN_NAME)
       @cart_form.submit(submit_btn)
     end
   end
 
   # Product detail page scraper
   class ProductPageScraper
-    SELECTOR_PRICE = 'span#priceblock_ourprice'.freeze
-    SELECTOR_TITLE = 'span#productTitle'.freeze
-    SELECTOR_IMAGES = 'div#altImages li.a-spacing-small.item img'.freeze
+    SELECTOR_PRICE = 'span#priceblock_ourprice'
+    SELECTOR_TITLE = 'span#productTitle'
+    SELECTOR_IMAGES = 'div#altImages li.a-spacing-small.item img'
     SELECTOR_FEATURES =
       'div#feature-bullets li:not(#replacementPartsFitmentBullet)'\
-      ' span.a-list-item'.freeze
-    SELECTOR_REVIEWS = 'span#acrCustomerReviewText'.freeze
-    SELECTOR_SELLER_RANK = 'div#detail-bullets li#SalesRank'.freeze
+      ' span.a-list-item'
+    SELECTOR_REVIEWS = 'span#acrCustomerReviewText'
+    SELECTOR_SELLER_RANK = 'div#detail-bullets li#SalesRank'
 
     def initialize(page)
       @page = page
